b6041dd206eb67c52abdf07c0ca98aef5d6ddf13
Alisher Uralov <aliwerdev@gmail.com>
2024-08-13 20:45:56 +0500

    fasdf

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,30 +4,25 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { CardModule } from './card/card.module';
 import { BoxModule } from './box/box.module';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
-import { FilesModule } from './files/files.module';
 import { CategoriesModule } from './categories/categories.module';
+// import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
-      isGlobal: true,
-    }),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'files') }),
+    ConfigModule.forRoot(),
+    // ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'files') }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     CardModule,
     BoxModule,
+    CategoriesModule,
     SharedModule,
     AuthModule,
     UserModule,
-    FilesModule,
-    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/files/dto/create-file.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateFileDto {
-  @ApiProperty()
-  file: string;
-}
